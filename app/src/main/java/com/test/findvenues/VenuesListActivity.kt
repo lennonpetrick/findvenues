e7de33409b1fc5f338e2d34154826bdd1ca61a1a
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-01-07 16:31:24 +0100

    Created project

---
+++ app/src/main/java/com/test/findvenues/VenuesListActivity.kt
@@ -0,0 +1,11 @@
+package com.test.findvenues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VenuesListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venues_list)
+    }
+}
