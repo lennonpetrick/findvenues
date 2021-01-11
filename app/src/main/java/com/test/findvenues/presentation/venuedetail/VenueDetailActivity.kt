ff8777dd9d2f6c30c3d2e40d518c2abf3ee5305d
Lennon Spirlandelli <lennonpetrick@gmail.com>
2021-01-11 17:24:02 +0100

    Created venue detail layout

---
+++ app/src/main/java/com/test/findvenues/presentation/venuedetail/VenueDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.test.findvenues.presentation.venuedetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.test.findvenues.R
+
+class VenueDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venue_detail)
+    }
+}
